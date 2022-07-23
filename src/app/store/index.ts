de95ec65c4f7e739bd70f660e87d3b02de479565
wangxun <wangxun@cyai.com>
2022-07-23 10:19:02 +0800

    feat:11.11-meta-reducer

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -2,11 +2,12 @@ import { ContextStoreModule } from './context/index';
 import { StoreModule } from '@ngrx/store';
 import { NgModule } from "@angular/core";
 import { EffectsModule } from '@ngrx/effects';
+import { metaReducers } from './configs';
 
 @NgModule({
   declarations:[],
   imports:[
-    StoreModule.forRoot({}),
+    StoreModule.forRoot({},{metaReducers}),
     EffectsModule.forRoot(),
     ContextStoreModule
   ]
