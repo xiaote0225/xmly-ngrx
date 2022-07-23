d8805a12ea24d9f4d28569f191b61bd2d0ac9426
wangxun <wangxun@cyai.com>
2022-07-23 10:52:06 +0800

    feat:11.13-运行时检查

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -2,21 +2,21 @@ import { ContextStoreModule } from './context/index';
 import { META_REDUCERS, StoreModule } from '@ngrx/store';
 import { NgModule } from "@angular/core";
 import { EffectsModule } from '@ngrx/effects';
-import { metaReducerFactory, metaReducers } from './configs';
+import { metaReducerFactory, STORE_CONFIG } from './configs';
 
 @NgModule({
   declarations:[],
   imports:[
-    StoreModule.forRoot({}),
+    StoreModule.forRoot({},STORE_CONFIG),
     EffectsModule.forRoot(),
     ContextStoreModule
   ],
-  providers:[
-    {
-      provide:META_REDUCERS,
-      useFactory:metaReducerFactory,
-      multi: true
-    }
-  ]
+  // providers:[
+  //   {
+  //     provide:META_REDUCERS,
+  //     useFactory:metaReducerFactory,
+  //     multi: true
+  //   }
+  // ]
 })
 export class XmStoreModule{}
